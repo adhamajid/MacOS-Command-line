c6f74809c8f387e8d05f0b7e1a6b170f97e99f8a
adhamajid <adha1900018252@webmail.uad.ac.id>
2024-09-19 03:16:05 +0700

    complete

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import Figlet
+
+@main
+struct FigletTool {
+  static func main() {
+    Figlet.say("Kill the boy n let the man be born")
+  }
+}
